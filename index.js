8f22f73032a55d1dd39c6716db97678382e3d307
Sam Tutton <sam@quiqup.com>
2018-03-24 21:48:43 +0000

    basic md5 checksum hash of images and create symlinks to global images folder

---
+++ index.js
@@ -0,0 +1,76 @@
+const path = require('path')
+const glob = require('glob')
+const fs = require('fs')
+const uuidv1 = require('uuid/v1')
+var crypto = require('crypto')
+
+const GLOBAL_IMAGE_DIR = './images'
+const GLOB_OPTIONS = {
+  ignore: [
+    './node_modules/**',
+  ],
+}
+
+let symlinkMap = require('./.image-dedupe-map.json')
+
+main()
+
+function main() {
+  findFiles()
+}
+
+function imagePath(image) {
+  return path.resolve('./assets/images/pickup.png')
+}
+
+function findFiles() {
+  glob('./**/*.png', GLOB_OPTIONS, (err, files) => {
+    const mappedFiles = files.map(file => readFile(file))
+    manageSymlinkMap(mappedFiles)
+  })
+}
+
+function manageSymlinkMap(symMaps) {
+  symMaps.forEach(symMap => {
+    if (!symlinkMap[symMap.checksum]) {
+      // add to symMap, move file and create Symlink
+      const originalPath = `${GLOBAL_IMAGE_DIR}/${symMap.checksum}.${symMap.path.split('.').pop()}`
+
+      console.log(symMap.path, originalPath)
+      fs.renameSync(symMap.path, originalPath)
+      addSymlink(originalPath, symMap.path)
+
+      symlinkMap[symMap.checksum] = {
+        paths: [symMap.path],
+        originalPath,
+      }
+    } else {
+      // link to already made - remove file and create Symlink
+      symlinkMap[symMap.checksum].paths.push(symMap.path)
+    }
+  })
+}
+
+function addSymlink(file, symLink) {
+  console.log('addSymlink:', file)
+
+  fs.symlink(file, symLink, res => {
+    console.log('Added a Symlink for:', symLink)
+  })
+}
+
+function readFile(path) {
+  const cs = checksum(fs.readFileSync(path))
+
+  return {
+    path,
+    checksum: cs,
+  }
+}
+
+function checksum(str) {
+  return crypto
+    .createHash('md5')
+    .update(str)
+    .digest('hex')
+}
