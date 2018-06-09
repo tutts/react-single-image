1ad9019d6e63862ff78f0aecbb0410da98636194
tutts <samueltutton@gmail.com>
2018-06-09 20:47:38 +0100

    move API specific modules under /api folder and clean up cli entry

--- cli.js
+++ cli.js
@@ -1,64 +1,9 @@
 const program = require('commander')
-const path = require('path')
-
-const package = require('./package.json')
-const singleOrigin = require('./single-origin')
-const create = require('./lib/create')
-const revert = require('./lib/revert')
-const update = require('./lib/update')
-
-const ROOT_DIR_REGEX = /<rootDir>/
-const ROOT_DIR = process.cwd()
-
-// CONFIG
-const defaultSettings = {
-  symlinks: true,
-  ignorePaths: ['./node_modules/**', './images'],
-  imageDir: './images',
-  matcher: './**/*.png',
-  mapFilename: 'map.json',
-}
-const settings = {
-  ...defaultSettings,
-  ...resolveRoot(package.singleOrigin),
-}
-
-function resolveRoot(settings) {
-  const resolvedSettings = {}
-  const root = settings.rootDir ? `${ROOT_DIR}/${settings.rootDir}` : ROOT_DIR
-
-  Object.keys(settings).forEach(setting => {
-    switch (typeof settings[setting]) {
-      case 'string': {
-        resolvedSettings[setting] = normalise(resolve(root, settings[setting]))
-        break
-      }
-      case 'object': {
-        if (Array.isArray(settings[setting])) {
-          resolvedSettings[setting] = settings[setting].map(location =>
-            normalise(resolve(root, location))
-          )
-          break
-        }
-      }
-      default:
-        resolvedSettings[setting] = settings[setting]
-    }
-  })
-
-  return resolvedSettings
-}
-
-function resolve(root, string) {
-  return string.replace(ROOT_DIR_REGEX, root)
-}
-
-function normalise(string) {
-  return path.normalize(string)
-}
+const { version } = require('./package.json')
+const { settings, create, revert, update } = require('./single-origin')
 
 program
-  .version(package.version)
+  .version(version)
   .option('-c, --create', 'Create your asset source map')
   .option('-u, --update', 'Update the asset source map')
   .option('-r, --revert', 'Revert your assets')
--- single-origin.js
+++ single-origin.js
@@ -1,1 +1,26 @@
-module.exports = {}
+const package = require('./package.json')
+const create = require('./lib/api/create')
+const revert = require('./lib/api/revert')
+const update = require('./lib/api/update')
+const { resolveRoot } = require('./lib/files/resolve')
+
+// CONFIG
+const defaultSettings = {
+  symlinks: true,
+  ignorePaths: ['./node_modules/**', './images'],
+  imageDir: './images',
+  matcher: './**/*.png',
+  mapFilename: 'map.json',
+}
+
+const settings = {
+  ...defaultSettings,
+  ...resolveRoot(package.singleOrigin),
+}
+
+module.exports = {
+  settings,
+  create,
+  revert,
+  update,
+}
